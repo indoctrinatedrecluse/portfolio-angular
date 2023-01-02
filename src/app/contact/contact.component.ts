27189799bf2d432f17c7c9700a2dbe1f5b1c09df
indoctrinatedrecluse <abmitra1999@gmail.com>
2023-01-02 03:51:40 -0800

    First Solid Draft

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  arrowPath: string;
+  arrowAltString: string;
+
+  constructor() {
+    this.arrowPath = "/assets/contact/down.jpg";
+    this.arrowAltString = " v ";
+  }
+
+  ngOnInit(): void {
+  }
 }
